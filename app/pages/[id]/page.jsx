f5dc727bf5fdc18bf07991c6bacc097bfb1962e8
Rakeshvanam6868 <https://github.com/Rakeshvanam6868>
2025-01-18 14:02:13 +0530

    Dynamic setup is finished

--- app/pages/[id]/page.jsx
+++ app/pages/[id]/page.jsx
@@ -6,12 +6,12 @@ import { ArrowRight, ArrowLeft } from "lucide-react";
 import Image from "next/image";
 import AudioPlayer from "../../../_components/AudioPlayer";
 import { useRouter } from "next/navigation";
-import PortraitLayout from "../../../_components/PortraitLayout";
+// import PortraitLayout from "../../../_components/PortraitLayout";
+import PortraitAudioPlayer from "./PortraitAudioPlayer";
 import graphQLClient from "@/lib/graphql-client";
 import { GET_PAGE_BY_SLUG } from "@/lib/queries";
 
 export async function getStaticPaths() {
-
   const paths = Array.from({ length: 27 }, (_, i) => ({
     params: { id: (i + 1).toString() }, // Pages 1 to 27
   }));
@@ -20,9 +20,11 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   const { id } = params;
-  
+
   try {
-    const response = await graphQLClient.request(GET_PAGE_BY_SLUG, { slug: id });
+    const response = await graphQLClient.request(GET_PAGE_BY_SLUG, {
+      slug: id,
+    });
     return {
       props: {
         pageData: response.page,
@@ -41,8 +43,8 @@ export async function getStaticProps({ params }) {
 export default function Page({ pageData }) {
   const [isExpanded, setIsExpanded] = useState(false);
   const [isPortrait, setIsPortrait] = useState(false);
-//   const router = useRouter();
-//   const { id } = router.query;
+  //   const router = useRouter();
+  //   const { id } = router.query;
   const router = useRouter();
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -77,7 +79,87 @@ export default function Page({ pageData }) {
   return (
     <div>
       {isPortrait ? (
-        <PortraitLayout />
+        <div className="w-full min-h-screen bg-cover select-none flex flex-col items-center justify-center ">
+          {/* Main Content Container */}
+          <div className="flex justify-center items-center">
+            <div className="grid grid-rows-2">
+              {/* Image Section */}
+              <div className="cursor-pointer flex justify-center items-center">
+                <div className="rounded -mt-16 h-[390px] xs:h-[100px] xs:w-[100px] sm:h-[350px] sm:w-[380px] md:h-[310px] md:w-[350px] lg:h-[450px] lg:w-[450px] xl:bg-white">
+                  <Image
+                    src={imgURL1}
+                    className="bg-white h-full w-full object-cover"
+                    alt="Cover Image"
+                    width={800}
+                    height={1400}
+                    unoptimized
+                  />
+                </div>
+              </div>
+
+              {/* Text Section */}
+              <div className="cursor-pointer overflow-hidden">
+                <div className="bgText  text-black">
+                  <div className="flex flex-col items-center justify-center xs:h-[100px] xs:w-[100px] sm:h-[350px] sm:w-[380px] md:h-[310px] md:w-[350px] lg:h-[450px] lg:w-[450px] xl:h-[500px] xl:w-[520px] md:p-6 xl:p-6 lg:p-6">
+                    <div
+                      className={`xl:py-12 sm:pr-10 max-h-[290px] xl:max-h-[460px] sm:pt-6 lg:pt-10 md:pt-10 md:pl-4 flex items-center justify-center flex-col gap-3 text-xl sm:text-sm md:text-base lg:text-lg xl:text-xl text-justify font-medium`}
+                    >
+                      <div
+                        className={`pr-2 mt-1 mb-2 mx-4 sm:pl-8 sm:w-[300px] md:w-[310px] lg:w-[380px] xl:w-[440px] ${
+                          isExpanded ? "overflow-auto" : "overflow-hidden"
+                        }`}
+                        style={{
+                          maxHeight: "250px",
+                          transition: "max-height 0.3s ease",
+                        }}
+                      >
+                        <p>
+                          Ten-year-old Veer is a very intelligent and curious
+                          boy. He and his Dadaji are great friends. They sleep
+                          in the same room. At bedtime, before falling asleep.
+                        </p>
+                        <p className="py-4">
+                          Dadaji makes up interesting stories on any topic that
+                          he feels little Veer should be aware of.
+                        </p>
+                        <p className="py-2">
+                          Veer loves to hear these stories. Veer&apos;s school
+                          was soon to hold a workshop on Space.
+                        </p>
+                      </div>
+                      <button
+                        onClick={toggleExpand}
+                        className="text-cyan-500 hover:text-cyan-700 focus:outline-none mb-6"
+                      >
+                        {isExpanded ? "Read Less" : "Read More"}
+                      </button>
+                    </div>
+
+                    {/* Audio Player Section */}
+                    <div className="flex justify-center items-center gap-3 mb-20">
+                      <button
+                        className="bg-white rounded-full p-2 text-black"
+                        onClick={pageClickPrevHandler}
+                      >
+                        <ArrowLeft />
+                      </button>
+                      <div className="sm:h-10 sm:mt-2 ">
+                        <PortraitAudioPlayer />
+                      </div>
+                      <button
+                        className="bg-white rounded-full p-2 text-black"
+                        onClick={pageClickNextHandler}
+                      >
+                        <ArrowRight />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        //Portrait END
       ) : (
         <div className="w-full min-h-screen bg-cover select-none flex flex-col items-center justify-center force-landscape">
           <div className="flex justify-center items-center min-h-[80vh]">
@@ -105,7 +187,11 @@ export default function Page({ pageData }) {
                           transition: "max-height 0.3s ease",
                         }}
                       >
-                        <p dangerouslySetInnerHTML={{ __html: pageData.content.text }} />
+                        <p
+                          dangerouslySetInnerHTML={{
+                            __html: pageData.content.text,
+                          }}
+                        />
                       </div>
                       <button
                         onClick={toggleExpand}
@@ -116,18 +202,14 @@ export default function Page({ pageData }) {
                     </div>
 
                     {/* Audio Player Section */}
-                    <AudioPlayer 
-                        
-                    />
+                    <AudioPlayer />
                   </div>
                 </div>
               </div>
 
               {/* Image Section */}
               <div className="cursor-pointer flex justify-center items-center">
-                <div
-                  className="rounded h-[300px] xs:h-[100px] xs:w-[100px] sm:h-[350px] sm:w-[400px] md:h-[310px] md:w-[350px] lg:h-[450px] lg:w-[450px] xl:h-[500px] xl:w-[520px] xl:bg-white"
-                >
+                <div className="rounded h-[300px] xs:h-[100px] xs:w-[100px] sm:h-[350px] sm:w-[400px] md:h-[310px] md:w-[350px] lg:h-[450px] lg:w-[450px] xl:h-[500px] xl:w-[520px] xl:bg-white">
                   <Image
                     src={imgURL}
                     className="bg-white h-full w-full object-cover"
